22d7f423fa59f8c77601a24867090ef79de943d5
CJGolding <goldingc30@gmail.com>
2024-05-18 13:37:37 +0100

    -Attempting to achieve loose decoupling.  -Branch currently in a non-working state

--- src/main.py
+++ src/main.py
@@ -12,16 +12,73 @@ class DataProcessor:
         product_data_path (str): The file path for the product sales data CSV.
         brand_data_path (str): The file path for the brand sales data CSV.
     """
-    def __init__(self, p_path: str, b_path: str):
+    def __init__(self, path: str):
         """
-        Initializes the DataProcessor class with paths to the product and brand data files.
+        Initializes the DataProcessor class with a path to the data file.
 
         Parameters:
-            p_path (str): Path to the product data CSV file.
-            b_path (str): Path to the brand data CSV file.
+            path (str): Path to the data CSV file.
         """
-        self.product_data_path = p_path
-        self.brand_data_path = b_path
+        self.data_path = path
+        self.data_frame = None
+        self.column_data_types = None
+        self.data_specific_attributes = None
+        self.default_columns = ['period_id', 'period_name']
+        self.columns_to_drop = self.default_columns + ['gross_sales', 'units_sold', 'prev_gross_sales', 'prev_units_sold']
+        self.new_column_order = ['current_week_commencing_date', 'previous_week_commencing_date',
+                                 'perc_gross_sales_growth', 'perc_unit_sales_growth']
+        self.merge_on_columns = self.default_columns + ['previous_week_commencing_date', 'current_week_commencing_date']
+        self.values_to_merge = self.default_columns + ['prev_gross_sales', 'prev_units_sold',
+                                                       'previous_week_commencing_date', 'current_week_commencing_date']
+
+
+    def _load_data(self):
+        self.data_frame = pd.read_csv(self.data_path, parse_dates=['week_commencing_date'], dayfirst=True)
+
+    def _merge_data(self):
+        df_previous = self.data_frame[self.data_frame['period_name'] == 'previous']
+        self.data_frame['previous_week_commencing_date'] = self.data_frame['week_commencing_date'] - pd.DateOffset(
+            years=1)
+        self.data_frame.rename(columns={'week_commencing_date': 'current_week_commencing_date'})
+        df_previous = df_previous.rename(columns={'week_commencing_date': 'previous_week_commencing_date',
+                                                  'gross_sales': 'prev_gross_sales',
+                                                  'units_sold': 'prev_units_sold'})
+
+        df_previous['current_week_commencing_date'] = df_previous['previous_week_commencing_date'] + pd.DateOffset(
+            years=1)
+        df_previous['period_name'] = 'current'
+        df_previous['period_id'] = 2
+        self.data_frame.merge(df_previous[self.values_to_merge], how='outer', on=self.merge_on_columns)
+        self.data_frame.fillna(0)
+        self.data_frame.astype(self.column_data_types)
+        self.data_frame = self.data_frame[self.data_frame['period_name'] == 'current']
+
+    def _calc_growth(self) -> None:
+        """
+        Calculates growth metrics for the data.
+
+        Parameters:
+            df (pd.DataFrame): The DataFrame containing sales data.
+
+        Returns:
+            pd.DataFrame: The DataFrame with growth metrics added.
+        """
+        self.data_frame['perc_gross_sales_growth'] = np.where(self.data_frame['prev_gross_sales'] > 0, np.round(((self.data_frame['gross_sales'] - self.data_frame['prev_gross_sales']) / self.data_frame['prev_gross_sales']) * 100, 2), None)
+        self.data_frame['perc_unit_sales_growth'] = np.where(self.data_frame['prev_units_sold'] > 0, np.round(((self.data_frame['units_sold'] - self.data_frame['prev_units_sold']) / self.data_frame['prev_units_sold']) * 100, 2), None)
+
+    @staticmethod
+    def prepare_dataframe(path):
+        pass
+
+    def _reorder_columns(self) -> None:
+        self.data_frame.reindex(columns=self.new_column_order)
+
+    def _sort_rows(self):
+        pass
+
+    def _df_to_dict(self):
+        self.dict = self.data_frame.to_dict(orient='records')
+
 
     def df_to_json(self, product: pd.DataFrame, brand: pd.DataFrame) -> None:
         """
@@ -31,15 +88,14 @@ class DataProcessor:
             product (pd.DataFrame): The processed product data.
             brand (pd.DataFrame): The processed brand data.
         """
-        product, brand = (self._convert_date_to_string(product, 'current_week_commencing_date'),
-                          self._convert_date_to_string(brand, 'current_week_commencing_date'))
+
         dict_product = product.sort_values(['product_name', 'current_week_commencing_date']).to_dict(orient='records')
         dict_brand = brand.sort_values(['brand_name', 'current_week_commencing_date']).to_dict(orient='records')
         dict_combined = {'PRODUCT': dict_product, 'BRAND': dict_brand}
         with open('C:/Users/goldi/backend-coding-challenge/output/results.json', 'w') as results_json:
             simplejson.dump(dict_combined, results_json, indent=4, ignore_nan=True)
 
-    def _convert_date_to_string(self, df: pd.DataFrame, current_format: str) -> pd.DataFrame:
+    def _convert_date_to_string(self, column_name: list) -> None:
         """
         Converts date columns in the DataFrame to string format, readable in a JSON file.
 
@@ -51,89 +107,10 @@ class DataProcessor:
         Returns:
             pd.DataFrame: The DataFrame with date columns converted to string format.
         """
-        df[current_format] = df[current_format].dt.strftime('%d/%m/%Y')
-        if 'previous_week_commencing_date' in df.columns:
-            df['previous_week_commencing_date'] = df['previous_week_commencing_date'].dt.strftime('%d/%m/%Y')
-        return df
+        for each in column_name:
+            self.data_frame[each] = self.data_frame[each].dt.strftime('%d/%m/%Y')
 
-    def process_data(self) -> tuple:
-        """
-        Processes the product and brand data files, calculates growth metrics, and prepares the data.
-
-        Returns:
-            tuple: A tuple containing two DataFrames, the processed product data and brand data.
-        """
-        df_product = self._prepare_data(self.product_data_path, ['barcode_no', 'product_name'],
-                                        defaultdict(lambda: object, period_id=np.int32, gross_sales=np.float64,
-                                                    units_sold=np.int32, prev_gross_sales=np.float64,
-                                                    prev_units_sold=np.int32, barcode_no=str))
-
-        df_brand = self._prepare_data(self.brand_data_path, ['brand_id', 'brand'],
-                                      defaultdict(lambda: object, period_id=np.int32, gross_sales=np.float64,
-                                                  units_sold=np.int32, brand_id=np.int32, prev_gross_sales=np.float64,
-                                                  prev_units_sold=np.int32))
-
-        df_brand = self._calc_growth(df_brand)
-        df_product = self._calc_growth(df_product)
-
-        df_brand = self._drop_columns_and_reorder(df_brand, ['brand_id', 'brand'])
-        df_product = self._drop_columns_and_reorder(df_product, ['barcode_no', 'product_name'])
-
-        return df_product, df_brand.rename(columns={'brand': 'brand_name'})
-
-    def _prepare_data(self, path: str, d_type: list, c_type: defaultdict) -> pd.DataFrame:
-        """
-        Prepares the data for processing by reading the CSV file, handling dates, and merging data entries to provide
-        current and previous entries into a single entry.
-
-        Parameters:
-            path (str): The file path to the data CSV.
-            d_type (list): A list of columns for data types.
-            c_type (defaultdict): A defaultdict specifying the column data types.
-
-        Returns:
-            pd.DataFrame: The prepared data DataFrame of either the product or the brand.
-        """
-        df = pd.read_csv(path, parse_dates=['week_commencing_date'], dayfirst=True)
-        df_previous = df[df['period_name'] == 'previous']
-
-        df['previous_week_commencing_date'] = df['week_commencing_date'] - pd.DateOffset(years=1)
-        df = df.rename(columns={'week_commencing_date': 'current_week_commencing_date'})
-        df_previous = df_previous.rename(columns={'week_commencing_date': 'previous_week_commencing_date',
-                                                  'gross_sales': 'prev_gross_sales',
-                                                  'units_sold': 'prev_units_sold'})
-
-        df_previous['current_week_commencing_date'] = df_previous['previous_week_commencing_date'] + pd.DateOffset(
-            years=1)
-        df_previous['period_name'] = 'current'
-        df_previous['period_id'] = 2
-
-        merge_values = ['previous_week_commencing_date', 'prev_gross_sales', 'current_week_commencing_date',
-                        'period_name',
-                        'period_id', 'prev_units_sold']
-        df = pd.merge(df, df_previous[merge_values + d_type], how='outer',
-                      on=['previous_week_commencing_date', 'current_week_commencing_date', 'period_name',
-                          'period_id'] + d_type)
-        df = df.fillna(0)
-        df = df.astype(c_type)
-
-        return df[df['period_name'] == 'current']
-
-    def _calc_growth(self, df: pd.DataFrame) -> pd.DataFrame:
-        """
-        Calculates growth metrics for the data.
-
-        Parameters:
-            df (pd.DataFrame): The DataFrame containing sales data.
-
-        Returns:
-            pd.DataFrame: The DataFrame with growth metrics added.
-        """
-        df['perc_gross_sales_growth'] = np.where(df['prev_gross_sales'] > 0, np.round(((df['gross_sales'] - df['prev_gross_sales']) / df['prev_gross_sales']) * 100, 2), None)
-        df['perc_unit_sales_growth'] = np.where(df['prev_units_sold'] > 0, np.round(((df['units_sold'] - df['prev_units_sold']) / df['prev_units_sold']) * 100, 2), None)
-        return df
-
-    def _drop_columns_and_reorder(self, df: pd.DataFrame, specific_order: list) -> pd.DataFrame:
+    def _drop_columns(self) -> None:
         """
         Drops unnecessary columns and reorders the remaining columns in the DataFrame to the specified order.
 
@@ -145,19 +122,102 @@ class DataProcessor:
         Returns:
             pd.DataFrame: The DataFrame with columns dropped and reordered.
         """
-        generic_order = ['current_week_commencing_date', 'previous_week_commencing_date', 'perc_gross_sales_growth',
-                         'perc_unit_sales_growth']
-        return df.drop(['period_id', 'period_name', 'gross_sales', 'units_sold', 'prev_gross_sales', 'prev_units_sold'],
-                       axis=1).reindex(columns=specific_order + generic_order)
+        self.data_frame.drop(self.columns_to_drop, axis=1)
 
+    def _return_dict(self):
+        return self.data_frame.to_dict(orient='records')
+
+class BrandProcessor(DataProcessor):
+
+    def __init__(self, path):
+        super().__init__(path)
+        self.data_specific_attributes = ['brand_id', 'brand']
+        self.new_column_order = self.data_specific_attributes + self.new_column_order
+        self.column_data_types = defaultdict(lambda: object, period_id=np.int32, gross_sales=np.float64,
+                                             units_sold=np.int32, brand_id=np.int32, prev_gross_sales=np.float64,
+                                             prev_units_sold=np.int32)
+        self.merge_on_columns += self.data_specific_attributes
+        self.values_to_merge += self.data_specific_attributes
+
+    def _reorder_columns(self) -> None:
+        self.data_frame.reindex(columns=self.new_column_order)
+        self.data_frame.rename(columns={'brand': 'brand_name'})
+
+    def _sort_rows(self):
+        self.data_frame.sort_values(['brand_name', 'current_week_commencing_date'])
+
+
+    @staticmethod
+    def prepare_dataframe(path) -> dict:
+        """
+        Processes the product and brand data files, calculates growth metrics, and prepares the data.
+
+        Returns:
+            tuple: A tuple containing two DataFrames, the processed product data and brand data.
+        """
+        brand_processor = BrandProcessor(path)
+        brand_processor._load_data()
+        brand_processor._merge_data()
+        brand_processor._calc_growth()
+        brand_processor._drop_columns()
+        brand_processor._reorder_columns()
+        brand_processor._convert_date_to_string(['current_week_commencing_date', 'previous_week_commencing_date'])
+        return brand_processor._return_dict()
+
+
+class ProductProcessor(DataProcessor):
+
+    def __init__(self, path):
+        super().__init__(path)
+        self.data_specific_attributes = ['barcode_no', 'product_name']
+        self.new_column_order = self.data_specific_attributes + self.new_column_order
+        self.column_data_types = defaultdict(lambda: object, period_id=np.int32, gross_sales=np.float64,
+                                             units_sold=np.int32, brand_id=np.int32, prev_gross_sales=np.float64,
+                                             prev_units_sold=np.int32)
+        self.merge_on_columns += self.data_specific_attributes
+        self.values_to_merge += self.data_specific_attributes
+
+    def _reorder_columns(self) -> None:
+        self.data_frame.reindex(columns=self.new_column_order)
+
+    def _sort_rows(self):
+        self.data_frame.sort_values(['product_name', 'current_week_commencing_date'])
+
+    @staticmethod
+    def prepare_dataframe(path) -> dict:
+        """
+        Processes the product and brand data files, calculates growth metrics, and prepares the data.
+
+        Returns:
+            tuple: A tuple containing two DataFrames, the processed product data and brand data.
+        """
+        product_processor = ProductProcessor(path)
+        product_processor._load_data()
+        product_processor._merge_data()
+        product_processor._calc_growth()
+        product_processor._drop_columns()
+        product_processor._reorder_columns()
+        product_processor._convert_date_to_string(['current_week_commencing_date', 'previous_week_commencing_date'])
+        return product_processor._return_dict()
+
+def merge_dict(dict_p, dict_b):
+    return {'PRODUCT': dict_p, 'BRAND': dict_b}
+
+
+def dict_to_json(dict_combined, output_path='../output/results.json'):
+    with open(output_path, 'w') as results_json:
+        simplejson.dump(dict_combined, results_json, indent=4, ignore_nan=True)
 
 def run(prod_path='../data/sales_product.csv', brand_path='../data/sales_brand.csv'):
     """
     Runs the data processing and produces an output file in the output/ directory.
     """
-    data_processor = DataProcessor(prod_path, brand_path)
-    df_product, df_brand = data_processor.process_data()
-    data_processor.df_to_json(df_product, df_brand)
+    product_dict = ProductProcessor.prepare_dataframe(prod_path)
+    brand_dict = BrandProcessor.prepare_dataframe(brand_path)
+    merged_data = merge_dict(product_dict, brand_dict)
+    dict_to_json(merged_data)
+
+
 
 
 if __name__ == "__main__":
