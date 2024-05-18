3f9b78560950da773cf1e8031bcd2ee17b52970f
CJGolding <goldingc30@gmail.com>
2024-05-18 23:17:19 +0100

    -Updated merge_dict and dict_to_json docstrings.

--- src/main.py
+++ src/main.py
@@ -4,10 +4,27 @@ import simplejson
 
 
 def merge_dict(dict_p: dict, dict_b: dict) -> dict:
+    """
+    Merges product and brand dictionaries into a single dictionary.
+
+    Parameters:
+        dict_p (dict): Product data dictionary.
+        dict_b (dict): Brand data dictionary.
+
+    Returns:
+        dict: Merged product and data dictionary.
+    """
     return {'PRODUCT': dict_p, 'BRAND': dict_b}
 
 
-def dict_to_json(dict_combined: dict, output_path) -> None:
+def dict_to_json(dict_combined: dict, output_path: str) -> None:
+    """
+    Converts dictionary to JSON format.
+
+    Parameters:
+        dict_combined (dict): Merged product and data dictionary.
+        output_path (str): Location of where JSON should be saved.
+    """
     with open(output_path, 'w') as results_json:
         simplejson.dump(dict_combined, results_json, indent=4, ignore_nan=True)
 
